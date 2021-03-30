30e8028c3b7dfa7ad193395f8f291dae2688635f
bobluppes <bobluppes@live.nl>
2021-03-30 13:51:53 +0200

    Added main file to data-generator

--- gen.py
+++ gen.py
@@ -8,87 +8,78 @@ from xeger import Xeger
 import progressbar
 import os
 
-# ------------|
-# PARAMETERS  |
-# ------------|
-#
-record_sizes = [1e3, 64e3, 1024e3]
-match_percentage = 0.05
-data_length = 100
-regex = '.*[tT][eE][rR][aA][tT][iI][dD][eE][ \t\n]+[dD][iI][vV][iI][nN][gG][ \t\n]+([sS][uU][bB])+[sS][uU][rR][fF][aA][cC][eE].*'
-parquet_chunksize = 1e6
-parquet_compression = 'none'
-outdir = './diving'
 
-# Print parameters at the start of each run
-print('\nDATASET GENERATOR')
-print('Records:\t' + str(record_sizes))
-print('Match percentage:\t' + str(match_percentage))
-print('Data length:\t' + str(data_length))
-print('Regular expression:\t' + repr(regex))
-print('Parquet chuncksize:\t' + str(parquet_chunksize))
-print('Parquet compression:\t' + parquet_compression + '\n')
+def regex_strings(record_sizes, match_percentage, data_length, regex, parquet_chunksize, parquet_compression, outdir):
 
-# Main loop for each of the requested record sizes
-print('Generating ' + str(len(record_sizes)) + ' datasets...\n')
-for i, records in enumerate(record_sizes):
+    # Print parameters at the start of each run
+    print('\nDATASET GENERATOR')
+    print('Records:\t' + str(record_sizes))
+    print('Match percentage:\t' + str(match_percentage))
+    print('Data length:\t' + str(data_length))
+    print('Regular expression:\t' + repr(regex))
+    print('Parquet chuncksize:\t' + str(parquet_chunksize))
+    print('Parquet compression:\t' + parquet_compression + '\n')
 
-    # Convert records to int and format to readable string
-    records = int(records)
-    if (records >= 1e6):
-        records_str = str(int(records / 1e6)) + 'M'
-    else:
-        records_str = str(records)
+    # Main loop for each of the requested record sizes
+    print('Generating ' + str(len(record_sizes)) + ' datasets...\n')
+    for i, records in enumerate(record_sizes):
 
-    # Create output directory if it does not yet exist
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
+        # Convert records to int and format to readable string
+        records = int(records)
+        if (records >= 1e6):
+            records_str = str(int(records / 1e6)) + 'M'
+        else:
+            records_str = str(records)
 
-    # Generate data and write to csv file
-    file = outdir + '/data-' + str(records_str) + '.csv'
-    print('Generating ' + file)
-    with open(file, mode='w') as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        # Create output directory if it does not yet exist
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
 
-        # Write the header row
-        writer.writerow(['value', 'string'])
+        # Generate data and write to csv file
+        file = outdir + '/data-' + str(records_str) + '.csv'
+        print('Generating ' + file)
+        with open(file, mode='w') as f:
+            writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-        # Generate each record
-        for j in progressbar.progressbar(range(records)):
+            # Write the header row
+            writer.writerow(['value', 'string'])
 
-            val = random.randint(1, 100)
+            # Generate each record
+            for j in progressbar.progressbar(range(records)):
 
-            # One in every (1/match_percentage) records should match the regex
-            if (j % (1 / match_percentage) == 0):
-                x = Xeger(limit=10)     # Limit repeats of regex operators such as * and + to 10
-                data = x.xeger(regex)
-                # Add padding such that the length of the data string is equal to the requested length
-                data += ''.join(random.choices(string.ascii_uppercase + string.ascii_uppercase + string.digits + string.whitespace, k=max(0, data_length - len(data))))
-            else:
-                data = ''.join(random.choices(string.ascii_uppercase + string.ascii_uppercase + string.digits + string.whitespace, k=data_length))
+                val = random.randint(1, 100)
 
-            # Write the generated record
-            writer.writerow([val, repr(data)])
+                # One in every (1/match_percentage) records should match the regex
+                if (j % (1 / match_percentage) == 0):
+                    x = Xeger(limit=10)     # Limit repeats of regex operators such as * and + to 10
+                    data = x.xeger(regex)
+                    # Add padding such that the length of the data string is equal to the requested length
+                    data += ''.join(random.choices(string.ascii_uppercase + string.ascii_uppercase + string.digits + string.whitespace, k=max(0, data_length - len(data))))
+                else:
+                    data = ''.join(random.choices(string.ascii_uppercase + string.ascii_uppercase + string.digits + string.whitespace, k=data_length))
 
-    # Convert the csv to a parquet file
-    parquet_file = outdir + '/data-' + str(records_str) + '.parquet'
-    csv_stream = pd.read_csv(file, chunksize=parquet_chunksize, low_memory=False)
-    print('Converting to ' + parquet_file)
-    for j, chunk in progressbar.progressbar(enumerate(csv_stream)):
+                # Write the generated record
+                writer.writerow([val, repr(data)])
 
-        # Initialize the parquet writer on the first chunk
-        if j == 0:
+        # Convert the csv to a parquet file
+        parquet_file = outdir + '/data-' + str(records_str) + '.parquet'
+        csv_stream = pd.read_csv(file, chunksize=parquet_chunksize, low_memory=False)
+        print('Converting to ' + parquet_file)
+        for j, chunk in progressbar.progressbar(enumerate(csv_stream)):
 
-            # Guess the schema of the csv file
-            df = pa.Table.from_pandas(df=chunk)
-            parquet_schema = df.schema
+            # Initialize the parquet writer on the first chunk
+            if j == 0:
 
-            # Initialize a Parquet writer
-            parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression=parquet_compression)
+                # Guess the schema of the csv file
+                df = pa.Table.from_pandas(df=chunk)
+                parquet_schema = df.schema
 
-        # Write csv chunk to the parquet file
-        table = pa.Table.from_pandas(chunk, schema=parquet_schema)
-        parquet_writer.write_table(table)
+                # Initialize a Parquet writer
+                parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression=parquet_compression)
 
-    # Close parquet writer after each chunk has been written
-    parquet_writer.close()
+            # Write csv chunk to the parquet file
+            table = pa.Table.from_pandas(chunk, schema=parquet_schema)
+            parquet_writer.write_table(table)
+
+        # Close parquet writer after each chunk has been written
+        parquet_writer.close()
---
+++ main.py
@@ -0,0 +1,14 @@
+import gen
+
+if __name__ == '__main__':
+
+    # Parameters
+    record_sizes = [10e6, 50e6]
+    match_percentage = 0.05
+    data_length = 100
+    regex = '.*[tT][eE][rR][aA][tT][iI][dD][eE][ \t\n]+[dD][iI][vV][iI][nN][gG][ \t\n]+([sS][uU][bB])+[sS][uU][rR][fF][aA][cC][eE].*'
+    parquet_chunksize = 1e6
+    parquet_compression = 'none'
+    outdir = './diving'
+
+    gen.regex_strings(record_sizes, match_percentage, data_length, regex, parquet_chunksize, parquet_compression, outdir)
