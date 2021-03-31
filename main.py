41166f96c86ee3ec2da16f15da77a3e9c4d40b75
Bob Luppes <bobluppes@live.nl>
2021-03-31 16:28:41 +0200

    Set correct dataset sizes for tests on AWS

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import gen
 if __name__ == '__main__':
 
     # Parameters
-    record_sizes = [10e6, 50e6]
+    record_sizes = [1e3, 64e3, 1024e3, 2048e3]
     match_percentage = 0.05
     data_length = 100
     regex = '.*[tT][eE][rR][aA][tT][iI][dD][eE][ \t\n]+[dD][iI][vV][iI][nN][gG][ \t\n]+([sS][uU][bB])+[sS][uU][rR][fF][aA][cC][eE].*'
